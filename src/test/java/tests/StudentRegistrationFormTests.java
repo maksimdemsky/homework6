0620838a59d4c7462ac120885f0f1cc4a603f33e
Администратор <maksimdemsky@gmail.com>
2021-04-11 19:46:44 +0300

    added negative tests

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -25,7 +25,8 @@ public class StudentRegistrationFormTests extends TestBase {
             stateAndCity = "NCR Delhi",
             fullName = firstName + " " + lastName,
             hobbies = "Sports, Music",
-            acceptFormText = "Thanks for submitting the form1",
+            acceptFormText = "Thanks for submitting the form",
+            acceptFormTextNegative = "Thanks for submitting the form1",
             sex = "Male",
             subject = "Physics";
 
@@ -69,6 +70,59 @@ public class StudentRegistrationFormTests extends TestBase {
         });
         step("Check Data Form", () -> {
             $(".modal-content").shouldHave(text(acceptFormText),
+                    text(fullName),
+                    text(userEmail),
+                    text(mobile),
+                    text("Male"),
+                    text(birth),
+                    text("Physics"),
+                    text(hobbies),
+                    text("123.jpg"),
+                    text(address),
+                    text(stateAndCity));
+        });
+    }
+
+    @Test
+    public void openDemoQaSiteAndCheckFormNegative() {
+        step("Open students registration form", () -> open("https://demoqa.com/automation-practice-form"));
+
+        step("Student Fill Data", () -> {
+            step("Fill common data", () -> {
+                $("#firstName").setValue(firstName);
+                $("#lastName").setValue(lastName);
+                $("#userEmail").setValue(userEmail);
+                $(byText(sex)).click();
+                $("#userNumber").setValue(mobile);
+            });
+            step("Set Birth Date", () -> {
+                $("#dateOfBirthInput").click();
+                $(".react-datepicker__month-select").selectOption("March");
+                $(".react-datepicker__year-select").selectOption("1997");
+                $(".react-datepicker__month").$(byText("7")).click();
+            });
+            step("Set Subject", () -> {
+                $("#subjectsInput").setValue(subject).pressEnter();
+            });
+            step("Set Hobbies", () -> {
+                $("#hobbiesWrapper").$(byText(hobby1)).click();
+                $("#hobbiesWrapper").$(byText(hobby2)).click();
+            });
+            step("Upload Pic", () -> {
+                $("#uploadPicture").uploadFromClasspath("123.jpg");
+            });
+            step("Set Address", () -> {
+                $("#currentAddress").setValue(address);
+                $("#stateCity-wrapper").scrollTo().$(byText("Select State")).click();
+                $(byText(state)).click();
+                $("#stateCity-wrapper").$(byText("Select City")).click();
+                $(byText(city)).click();
+            });
+            step("Submit form", () ->
+                    $("#submit").click());
+        });
+        step("Check Data Form", () -> {
+            $(".modal-content").shouldHave(text(acceptFormTextNegative),
                     text(fullName),
                     text(userEmail),
                     text(mobile),
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentHelper.*;
 
@@ -26,5 +27,6 @@ public class TestBase {
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
+        closeWebDriver();
     }
 }
